d63098a814385c8b3a4107ca40354c180529df91
igorrm19 <igormachado14@gmail.com>
2025-09-12 11:30:06 -0300

    criando tela inicial

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header/header.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, NgOptimizedImage],
   providers: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
